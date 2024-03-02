0aada18cba03546bd18ead68c86bdfc738bdea46
rajindersinghsodhi <rajsodhisocial@gmail.com>
2024-03-02 14:15:41 +0530

    Home Icon highlighted

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import { useState, useEffect } from "react"; 
 import logo from "../logo.png";
 import './Header.scss';
 import { CgProfile } from "react-icons/cg";
@@ -8,17 +8,28 @@ import { PiTelevisionSimple } from "react-icons/pi";
 import { MdOutlineSportsSoccer } from "react-icons/md";
 import { TbCategoryPlus } from "react-icons/tb";
 import { BiCameraMovie } from "react-icons/bi";
-
-// import {Link} from "react-router-dom";
+import { useLocation } from 'react-router-dom';
 
 const Header = () => {
+
+  const location = useLocation();
+  const [isHomePage, setIsHomePage] = useState(false);
+
+  useEffect(() => {
+    setIsHomePage(location.pathname === '/');
+  }, [location.pathname]);
+
   return (
     <nav className="header">
         <img src={logo} alt="" />
         <div className="icons">
             <CgProfile className='glarge'/>
             <IoSearchOutline className='glarge'/>
-            <GoHomeFill className='glarge'/>
+            {isHomePage ? (
+              <GoHomeFill className='glarge' style={{ color: 'white', transform : "Scale(1.2)" }} />
+            ) : (
+              <GoHomeFill className='glarge' />
+            )}
             <PiTelevisionSimple className='glarge'/>
             <BiCameraMovie className='glarge'/>
             <MdOutlineSportsSoccer className='glarge'/>
